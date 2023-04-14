392bcf897098df580cb560f1c07aaa82ab242910
Ana Finotelli <finotelli2004@gmail.com>
2023-04-13 22:38:36 -0300

    divisao.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let dividendo = prompt("Qual o valor a ser dividido?:");
+let divisor = prompt("por quantos?:");
+quociente = dividendo / divisor
+    
+alert(`${dividendo} ÷ ${divisor} = ${quociente}`);
